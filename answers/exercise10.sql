222f434b72020f5ccbb669ce75b418cb321025d2
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-22 23:10:01 -0400

    Finished first 10 SQL exercises.

---
+++ answers/exercise10.sql
@@ -0,0 +1,2 @@
+ALTER table Users ADD column MiddleName VARCHAR (255), ADD column NickName VARCHAR (255), ADD column Email VARCHAR (255),
+ADD column Suffix VARCHAR (64), ADD column BadgeID VARCHAR (255);
---
+++ answers/exercise8.sql
@@ -0,0 +1,1 @@
+ALTER TABLE Users ADD column Birthday DATE;
